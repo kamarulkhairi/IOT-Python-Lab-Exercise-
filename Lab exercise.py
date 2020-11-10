7f8aca19c7acef8e3446abefe3067b8f96a72fa7
Kamarul Khairi <kamarulkhairi@gmail.com>
2020-11-10 11:31:36 +0800

    Insert

--- Lab exercise.py
+++ Lab exercise.py
@@ -1,8 +1,12 @@
 class Square:
     side = 1.0
 
-    def setSide(self):
+
+def __init__(self, side):
+    self.side = side
+
+    def setSide(insert):
         if self < 0:
             self.side = 0
         else:
-            self.side = self
+            self.side = input
